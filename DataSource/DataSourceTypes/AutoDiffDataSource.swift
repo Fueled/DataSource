a8c79d0795195330d628415453ef7d2a28c5cc05
Vadim Yelagin <vadim@fueled.com>
2015-08-14 18:10:08 +0600

    Expose `findMoves` parameter in `AutoDiffDataSource` initializer

--- DataSource/DataSourceTypes/AutoDiffDataSource.swift
+++ DataSource/DataSourceTypes/AutoDiffDataSource.swift
@@ -21,13 +21,13 @@ public final class AutoDiffDataSource<T>: DataSource {
     
     public let compare: (T, T) -> Bool
     
-    public init(_ items: [T] = [], supplementaryItems: [String: Any] = [:], _ compare: (T, T) -> Bool) {
+    public init(_ items: [T] = [], supplementaryItems: [String: Any] = [:], findMoves: Bool = true, _ compare: (T, T) -> Bool) {
         (self.changes, self.observer) = Signal<DataChange, NoError>.pipe()
         self.items = MutableProperty(items)
         self.supplementaryItems = supplementaryItems
         self.compare = compare
         func autoDiff(old: [T], new: [T]) -> DataChange {
-            return AutoDiff.compare(old: old, new: new, findMoves: true, compare: compare).toItemChanges()
+            return AutoDiff.compare(old: old, new: new, findMoves: findMoves, compare: compare).toItemChanges()
         }
         self.disposable = self.items.producer
             |> combinePrevious(items)
