82ee82496ef8405d3fdfe263ca9e77bff5c7e2f9
Aleksei Bobrov <aleksei@fueled.com>
2019-01-31 13:46:32 +0700

    fix(celldescriptor): code refactoring

--- DataSource/DataSourceCellDescriptor.swift
+++ DataSource/DataSourceCellDescriptor.swift
@@ -1,19 +1,13 @@
 import UIKit
 
-enum CellPrototypeSource {
-	case storyboard
-	case nib(UINib)
-	case `class`(AnyObject.Type)
-}
-
 struct CellDescriptor {
 	let reuseIdentifier: String
-	let prototypeSource: CellPrototypeSource
+	let prototypeSource: PrototypeSource
 	let isMatching: (IndexPath, Any) -> Bool
 
 	init(
 		_ reuseIdentifier: String,
-		_ prototypeSource: CellPrototypeSource = .storyboard,
+		_ prototypeSource: PrototypeSource = .storyboard,
 		isMatching: @escaping (IndexPath, Any) -> Bool)
 	{
 		self.reuseIdentifier = reuseIdentifier
@@ -24,21 +18,27 @@ struct CellDescriptor {
 	init<Item>(
 		_ reuseIdentifier: String,
 		_ itemType: Item.Type,
-		_ prototypeSource: CellPrototypeSource = .storyboard)
+		_ prototypeSource: PrototypeSource = .storyboard)
 	{
 		self.init(reuseIdentifier, prototypeSource) { $1 is Item }
+	}
+}
+
+extension CellDescriptor {
+	enum PrototypeSource {
+		case storyboard
+		case nib(UINib)
+		case `class`(AnyObject.Type)
 	}
 }
 
 extension CollectionViewDataSource {
 	func configure(_ collectionView: UICollectionView, using cellDescriptors: [CellDescriptor]) {
 		self.reuseIdentifierForItem = { indexPath, item in
-			for descriptor in cellDescriptors {
-				if descriptor.isMatching(indexPath, item) {
-					return descriptor.reuseIdentifier
-				}
+			guard let reuseIdentifier = cellDescriptors.first(where: { $0.isMatching(indexPath,item) })?.reuseIdentifier else {
+				fatalError()
 			}
-			fatalError()
+			return reuseIdentifier
 		}
 		for descriptor in cellDescriptors {
 			switch descriptor.prototypeSource {
@@ -58,12 +58,10 @@ extension CollectionViewDataSource {
 extension TableViewDataSource {
 	func configure(_ tableView: UITableView, using cellDescriptors: [CellDescriptor]) {
 		self.reuseIdentifierForItem = { indexPath, item in
-			for descriptor in cellDescriptors {
-				if descriptor.isMatching(indexPath, item) {
-					return descriptor.reuseIdentifier
-				}
+			guard let reuseIdentifier = cellDescriptors.first(where: { $0.isMatching(indexPath,item) })?.reuseIdentifier else {
+				fatalError()
 			}
-			fatalError()
+			return reuseIdentifier
 		}
 		for descriptor in cellDescriptors {
 			switch descriptor.prototypeSource {
